bb2c6e8eda22276850f4e5c53aa645de5b50605f
Alyssa Hooper <nesteralyssa@gmail.com>
2020-05-27 16:42:04 -0700

    bootstrap react radio boxes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <input className="form-check" type="checkbox" value="" />
-      <p>Hi</p>
-
-      <input className="form-check" type="checkbox" value="" />
-      <p>I</p>
-
-      <input className="form-check" type="checkbox" value="" />
-      <p>Exist</p>
+      <div className="container">
+        <>
+          <Form.Check type="radio" aria-label="radio 1" />
+          <p>radio 1</p>
+          <Form.Check type="radio" aria-label="radio 2" />
+          <p>radio 2</p>
+          <Form.Check type="radio" aria-label="radio 3" />
+          <p>radio 3</p>
+          <Form.Check type="radio" aria-label="radio 4" />
+          <p>radio 4</p>
+        </>
+      </div>
     </div>
   );
 }
